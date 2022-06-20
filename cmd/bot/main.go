a32495737df6f33904264d4a7f7b30eb77ca1f33
Tikhonin Fedor Igorevich <ftikhonin@ozon.ru>
2022-06-20 22:22:49 +0300

    Moved the token to the env

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-const token = "TOKEN"
-
 func main() {
+	godotenv.Load()
+
+	token := os.Getenv("TOKEN")
+
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
